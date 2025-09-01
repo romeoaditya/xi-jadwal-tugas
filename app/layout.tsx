67603557ac5ff75f96cce394d4e51531ff800945
romeoaditya <romeoaditya22@gmail.com>
2025-09-01 08:39:36 +0700

    Add analystics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import {Geist, Geist_Mono} from "next/font/google";
+import {Analytics} from "@vercel/analytics/next";
 import "./globals.css";
 import {Toaster} from "sonner";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Toaster />
 
         {children}
+        <Analytics />
       </body>
     </html>
   );
